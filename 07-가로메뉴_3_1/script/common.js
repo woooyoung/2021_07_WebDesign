2a7dd34d62e43a008660d0429c68725cb9b63c3e
woooyoung <axdsw121@gmail.com>
2021-07-25 11:30:09 +0900

    가로메뉴_3_1

---
+++ 07-가로메뉴_3_1/script/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".menu > ul > li").mouseover(function () {
+        $(".menu > ul > li > ul").show();
+    });
+    $(".menu > ul > li").mouseout(function () {
+        $(".menu > ul > li > ul").hide();
+    });
+
+
+});
