cf289dcd6867f2c01a31fbf51eec02c7a2e11662
woooyoung <axdsw121@gmail.com>
2021-07-31 10:38:55 +0900

    레이아웃에 스크립트 연결 추가 & 팝업 기능 구현

---
+++ 09-팝업/script/common.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+    $(".shortcut").click(function () {
+        $(".layer").show();
+        $(".layer-bg").show();
+        //        $(".layer").css("display","block");
+    });
+
+    $(".close-btn").click(function () {
+        $(".layer").hide();
+        $(".layer-bg").hide();
+        //        $(".layer").css("display","none");
+    });
+});
