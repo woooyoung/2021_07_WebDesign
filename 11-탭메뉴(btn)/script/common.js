c1f1f9a9353e11ec98f1dbc71abf70fd739da490
woooyoung <axdsw121@gmail.com>
2021-08-01 11:15:37 +0900

    탭 메뉴(btn- show & hide)

---
+++ 11-탭메뉴(btn)/script/common.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".tab1").click(function () {
+        $(".notice").show();
+        $(".gallery").hide();
+    });
+    $(".tab2").click(function () {
+        $(".notice").hide();
+        $(".gallery").show();
+    });
+});
